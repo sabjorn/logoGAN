bf0a0a34cacd310ad75950f8bc7242e606c0379f
sabjorn <info@sabjorn.net>
2020-11-01 19:50:21 +0100

    update training script to pass in data generator

--- source/train.py
+++ source/train.py
@@ -1,9 +1,15 @@
-from Gan import Gan
+import argparse
 
-gan = Gan("../dataset/train",
-          imgDims=(128, 128, 1),
+from Gan import Gan
+from DataGenerator import DataGenerator
+
+IMG_DIMS = (128, 128, 1)
+DATA_PATH = "/data"
+data_generator = DataGenerator(IMG_DIMS, DATA_PATH, filetypes=['.png'], convert_bw=True)
+gan = Gan(data_generator=data_generator,
+          imgDims=IMG_DIMS,
           batchSize=64,
-          noiseDim=100)
+          noiseDims=100)
 
 gan.train(epochs=10000,
           checkpointFrequency=100)
