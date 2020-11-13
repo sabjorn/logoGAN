f79b286ba5aed4ac756f5ec513a727a7df3f95b0
sabjorn <info@sabjorn.net>
2020-11-13 16:59:57 +0100

    start making larger model

--- source/train.py
+++ source/train.py
@@ -2,7 +2,7 @@ import logging
 from DataGenerator import DataGenerator
 from Gan import Gan
 
-IMG_DIMS = (128, 128, 3)
+IMG_DIMS = (1080, 1080, 3)
 DATA_PATH = "/data"
 
 logging.basicConfig(level=logging.DEBUG)
