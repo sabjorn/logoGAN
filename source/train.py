3003da41f373edb8e1d9711324d0239fc47435ac
sabjorn <info@sabjorn.net>
2020-12-27 18:43:37 +0100

    setup experiment for 1024x1024 with large training set

--- source/train.py
+++ source/train.py
@@ -3,7 +3,7 @@ from DataGenerator import DataGenerator
 # from Gan import Gan
 from CVAE import CVAE
 
-IMG_DIMS = (256, 256, 3)
+IMG_DIMS = (1024, 1024, 3)
 DATA_PATH = "/data"
 BACKGROUND_COLOUR = (255, 255, 255)
 
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 data_generator = DataGenerator(IMG_DIMS, DATA_PATH, filetypes=['.png', '.jpg'], use_memmap=False, background_color=BACKGROUND_COLOUR, crop=False, load_from_disk=False)
 cvae = CVAE(data_generator=data_generator,
           input_shape=IMG_DIMS,
-          batch_size=32,
+          batch_size=2,
           latent_dim=16,
           seed=4)
 
