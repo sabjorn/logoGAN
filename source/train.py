a90a5ea8b9982abee19fc8a7927471f433df7cad
Steeve A. Bjornson <info@sabjorn.net>
2021-01-02 22:47:00 +0100

    training model for image size 2048x2048

--- source/train.py
+++ source/train.py
@@ -4,16 +4,16 @@ from DataGenerator import DataGenerator
 # from Gan import Gan
 from CVAE import CVAE, Sampling
 
-IMG_DIMS = (1024, 1024, 3)
+IMG_DIMS = (2048, 2048, 3)
 DATA_PATH = "/data"
 BACKGROUND_COLOUR = (255, 255, 255)
-PRETRAINED_ENCODER_PATH = "/data/models/encoder_at_epoch60.h5"
-PRETRAINED_DECODER_PATH = "/data/models/decoder_at_epoch60.h5"
+#PRETRAINED_ENCODER_PATH = "/data/models/encoder_at_epoch60.h5"
+#PRETRAINED_DECODER_PATH = "/data/models/decoder_at_epoch60.h5"
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-data_generator = DataGenerator(IMG_DIMS, DATA_PATH, filetypes=['.png', '.jpg'], use_memmap=False, background_color=BACKGROUND_COLOUR, crop=False, load_from_disk=False)
+data_generator = DataGenerator(IMG_DIMS, DATA_PATH, filetypes=['.png', '.jpg'], use_memmap=False, background_color=BACKGROUND_COLOUR, crop=False, load_from_disk=True)
 cvae = CVAE(data_generator=data_generator,
           input_shape=IMG_DIMS,
           batch_size=2,
