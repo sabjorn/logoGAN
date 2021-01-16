0cfdd5dbae5cb51f1a9881ca0f3f107034f330dc
Steeve A. Bjornson <info@sabjorn.net>
2021-01-16 14:05:12 +0100

    train on kathi paintings from deep white background IG at 75epoch (e61acb0)

--- source/train.py
+++ source/train.py
@@ -7,8 +7,8 @@ from CVAE import CVAE, Sampling
 IMG_DIMS = (1024, 1024, 3)
 DATA_PATH = "/data"
 BACKGROUND_COLOUR = (255, 255, 255)
-#PRETRAINED_ENCODER_PATH = "3003da4/saved_models/encoder_at_epoch150.h5"
-#PRETRAINED_DECODER_PATH = "3003da4/saved_models/decoder_at_epoch150.h5"
+PRETRAINED_ENCODER_PATH = "./e61acb0/saved_models/encoder_at_epoch75.h5"
+PRETRAINED_DECODER_PATH = "./e61acb0/saved_models/decoder_at_epoch75.h5"
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -21,17 +21,23 @@ cvae = CVAE(data_generator=data_generator,
           seed=4)
 
 try:
+  print(f"loading external model {PRETRAINED_ENCODER_PATH}")
   cvae.encoder = keras.models.load_model(PRETRAINED_ENCODER_PATH, {"Sampling":Sampling})
-except:
+except Exception as e:
+  print("no ENCODER model to load")
+  print(e)
   pass
 
 try:
+  print(f"loading external model {PRETRAINED_DECODER_PATH}")
   cvae.decoder = keras.models.load_model(PRETRAINED_DECODER_PATH)
-except:
+except Exception as e:
+  print("no DECODER model to load")
+  print(e)
   pass
 
-cvae.train(epochs=1000,
-          checkpoint_frequency = 100,
+cvae.train(epochs=150,
+          checkpoint_frequency = 10,
           num_checkpoint_image=5)
 
 
