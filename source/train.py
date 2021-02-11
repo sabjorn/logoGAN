ba43e648ad5f13511d2676c086a8639340758b53
Steeve A. Bjornson <info@sabjorn.net>
2021-02-11 10:03:45 +0100

    train with e61acb0 encoder_at_epoch180 using original large painting dataset

--- source/train.py
+++ source/train.py
@@ -7,8 +7,8 @@ from CVAE import CVAE, Sampling
 IMG_DIMS = (1024, 1024, 3)
 DATA_PATH = "/data"
 BACKGROUND_COLOUR = (255, 255, 255)
-PRETRAINED_ENCODER_PATH = "./3003da4/saved_models/encoder_at_epoch60.h5"
-PRETRAINED_DECODER_PATH = "./3003da4/saved_models/decoder_at_epoch60.h5"
+PRETRAINED_ENCODER_PATH = "./e61acb0/saved_models/encoder_at_epoch180.h5"
+PRETRAINED_DECODER_PATH = "./e61acb0/saved_models/decoder_at_epoch180.h5"
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
