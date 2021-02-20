04f9cb3a87a7ce8193b14621b304e9b189d01bb4
sabjorn <info@sabjorn.net>
2021-02-20 11:57:23 +0100

    setup for normalization step

--- source/train.py
+++ source/train.py
@@ -7,13 +7,13 @@ from CVAE import CVAE, Sampling
 IMG_DIMS = (1024, 1024, 3)
 DATA_PATH = "/data"
 BACKGROUND_COLOUR = (255, 255, 255)
-PRETRAINED_ENCODER_PATH = "./45a8eee/saved_models/encoder_at_epoch60.h5"
-PRETRAINED_DECODER_PATH = "./45a8eee/saved_models/eecoder_at_epoch60.h5"
+# PRETRAINED_ENCODER_PATH = "./45a8eee/saved_models/encoder_at_epoch60.h5"
+# PRETRAINED_DECODER_PATH = "./45a8eee/saved_models/eecoder_at_epoch60.h5"
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-data_generator = DataGenerator(IMG_DIMS, DATA_PATH, filetypes=['.png', '.jpg', '.tif'], use_memmap=False, background_color=BACKGROUND_COLOUR, crop=False, load_from_disk=True)
+data_generator = DataGenerator(IMG_DIMS, DATA_PATH, filetypes=['.png', '.jpg', '.tif'], use_memmap=False, background_color=BACKGROUND_COLOUR, crop=False, load_from_disk=False)
 cvae = CVAE(data_generator=data_generator,
           input_shape=IMG_DIMS,
           batch_size=2,
@@ -34,7 +34,7 @@ except NameError as e:
 except Exception as e:
   print(e)
 
-cvae.train(epochs=100,
+cvae.train(epochs=10000,
           checkpoint_frequency = 10,
           num_checkpoint_image=5)
 
